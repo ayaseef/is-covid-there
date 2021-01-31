de42a5eff52dc27c6b24efa6f38689de72a57036
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-01-31 14:24:55 -0800

    updated types for COVID Data APi

--- src/components/api/CovidDataApi.tsx
+++ src/components/api/CovidDataApi.tsx
@@ -2,10 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 type CovidDataState = {
-    County: string,
     State_name: string,
+    County: string,
     Cases_7_day_count_change: number,
-    deaths_7_day_count_change: number
+    deaths_7_day_count_change: number,
+    population_density_2019: number,
+    avg_hh_size: number,
+    percent_uninsured_2019: number,
+    poverty_rate_2019: number,
+    Percent_65_plus: number,
+    Case_death_start_date: Date, //just for us
+    Case_death_end_date: Date, //just for us
+    Testing_start_date: Date, //just for us
+    Testing_end_date: Date, //just for us
+    Hospital_data_collection_date: Date, //just for us
+    total_hospitals_reporting: number,
+    admissions_covid_confirmed_last_7_days: number,
+    admissions_covid_confirmed_last_7_days_per_100_beds: number,
+    percent_adult_inpatient_beds_used_confirmed_covid: number,
+    Hospitals_included_in_percent_adult_inpatient_beds_used_confirmed_covid: number, //just for us
+    percent_adult_icu_beds_used_confirmed_covid: number,
+    Hospitals_included_in_percent_adult_icu_beds_used_confirmed_covid: number, //just for us
+    percent_positive_14_day: number,
+    school_composite: string
+
 }
 
 const CovidData = () => {
