62c00791854fb1a63cc69200f79ee1a2b341d1e8
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-02-05 14:47:55 -0800

    fixed the issue with the naming of the dropdown of categories of covid cases

--- src/components/covid/CategorySearch.js
+++ src/components/covid/CategorySearch.js
@@ -37,6 +37,21 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
         'percent_adult_icu_beds_used_confirmed_covid',
         'percent_positive_14_day',
     ]
+
+    const categoriesObj = {
+        Cases_7_day_count_change: 'covid cases of 7 day count change',
+        deaths_7_day_count_change: 'death cases of 7 day count change',
+        population_density_2019: 'population density 2019',
+        avg_hh_size: 'average household size',
+        percent_uninsured_2019: 'precent of uninsured 2019',
+        poverty_rate_2019: 'poverty rate 2019',
+        total_hospitals_reporting: 'total reporting hospitals',
+        admissions_covid_confirmed_last_7_days: 'covid addmissions confirmed last 7 days',
+        admissions_covid_confirmed_last_7_days_per_100_beds: 'covid addmissions confirmed last 7 days per 100 beds',
+        percent_adult_inpatient_beds_used_confirmed_covid: 'precent of inpatient beds used for confirmed COVID adults cases',
+        percent_adult_icu_beds_used_confirmed_covid: 'precent of ICU beds used for confirmed COVID adults cases',
+        percent_positive_14_day: 'precent of positive tests of 14 days',
+    }
     // forwardRef again here!
     // Dropdown needs access to the DOM of the Menu to measure it
     const CustomMenu = React.forwardRef(
@@ -60,7 +75,7 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
             <ul className="list-unstyled">
             {React.Children.toArray(children).filter(
                 (child) =>
-                !value || child.props.children.toLowerCase().startsWith(value) || child.props.children.toUpperCase().startsWith(value),
+                !value || child.props.children.toLowerCase().startsWith(value),
             )}
             </ul>
         </div>
@@ -79,10 +94,17 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
         Select a Category <span></span>   
         </Dropdown.Toggle>
         {/* maybe here to fix the search? */}
-        <Dropdown.Menu as={CustomMenu}>
+        {/* <Dropdown.Menu as={CustomMenu}>
             {categories.map((category,i) => {
             return(
                 <Dropdown.Item eventKey={i + 1} onClick={(event)=>{props.onFieldSelected(category)}} >{category}</Dropdown.Item>
+            )
+            })}
+        </Dropdown.Menu> */}
+                <Dropdown.Menu as={CustomMenu}>
+            {Object.entries(categoriesObj).map(([key,value],i) => {
+            return(
+                <Dropdown.Item eventKey={i + 1} onClick={(event)=>{props.onFieldSelected(key)}} >{value}</Dropdown.Item>
             )
             })}
         </Dropdown.Menu>
