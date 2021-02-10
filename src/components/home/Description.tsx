5f8409c65c69acf66ef8d8162be624d7b8c8d39d
Ayesha Asif <ayaseef@gmail.com>
2021-02-10 12:45:28 -0800

    Attempted margin increase for the home page app description

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const AppDescription = () :any => {
     return(
-        <div>
+        <div className={'description'}>
             <br/>
             <br/>                
             {/* Always use proper punctuation and grammar.
