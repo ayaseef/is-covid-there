722cabc22291686e261208a2e58b3fcaa1ca86cf
Ayesha Asif <ayaseef@gmail.com>
2021-02-10 11:52:14 -0800

    Noor and Ayesha worked on the app description

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-// 
+
 const AppDescription = () :any => {
     return(
         <div>
-            <h2> Title </h2>
-            <p> This is an app to track covid-19 impact data as well as the vaccination solution to the covid impact around us in the United States </p>
+            <br/>
+            <br/>                
+            {/* Always use proper punctuation and grammar.
+            Introduce your app clearly and succinctly.
+            Describe your app's most notable features. 
+            Use conversational language.
+            Think about your target customer and highlight features that will appeal to that customer.
+            Explain how and why your app will benefit users.
+            Show off—don't forget to include the attributes that make your app stand out and feel free to do so in detail..  */}
+            
+            <p>Traveling or moving during COVID is such a tough decision to make! 
+            It's not easy to understand the COVID situation in a specific place inside the US as there is alot of confusing information.
+            'Is COVID There?' aims to present the spread of COVID-19 and vaccination data in an easy and user friendly manner. In the COVID section, there is also  additional information about communities for each county in the US.</p>
+
+            <p>This app has up-to-date information from Center of Disease Control and Prevention (CDC)</p>
+
+            <p>Please note that the recommendation during COVID is to stay home and not to travel, but in case of extreme emergency, we do hope 'Is COVID There?' will help in making the best decision for moving or traveling.</p>
+
+            <p>Please stay safe!</p>
         </div>
     )
 }
