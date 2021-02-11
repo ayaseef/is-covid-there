3787835a0e30195bd44b60494235d6a3d0869b86
Ayesha Asif <ayaseef@gmail.com>
2021-02-11 11:56:36 -0800

    Refreshed the description

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -13,17 +13,14 @@ const AppDescription = () :any => {
             Think about your target customer and highlight features that will appeal to that customer.
             Explain how and why your app will benefit users.
             Show off—don't forget to include the attributes that make your app stand out and feel free to do so in detail..  */}
-            {/* <p> Detalied Vaccine Data on Map - Hospitals Data on Map - School Responses data - Community Data on Map -  Reliable data from CDC</p> */}
+            <p className={'highlights'}> Detalied Vaccine Data on Map - Hospitals Data on Map - School Responses Data - Community Data on Map -  Reliable Data from CDC</p>
+            {/* <hr></hr> */}
+            <br></br>
             <p>Traveling or moving during COVID is such a tough decision to make! 
             It's not easy to understand the COVID situation in a specific place inside the US as there are lots of confusing information.</p>
 
             <p>'Is COVID There?' aims to present the spread of COVID-19 and vaccination data using up-to-date information from the Center of Disease Control and Prevention (CDC) in an easy and user-friendly manner.</p>
-            <ul className="featureList">
-                <li> 1 feature </li>
-                <li> 2 feature </li>
-                <li> 3 feature </li>
-            </ul>
-
+           
             <p>Please keep in mind, the recommendation during COVID is to stay home and not to travel. But in case of extreme emergency, we do hope that 'Is COVID There?' will help in making the best decision for moving or traveling.</p>
 
             <p>Please stay safe!</p>
