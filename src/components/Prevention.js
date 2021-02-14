59b0343503ac4b121e510989b37202e5de33ae98
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-02-14 15:23:26 -0800

    fixed the size issue with Prevention image

--- src/components/Prevention.js
+++ src/components/Prevention.js
@@ -4,8 +4,8 @@ import './Prevention.css';
 
 const PreventionComponenet = ()  => {
     return(
-        <div className={'image'}>
-            <img src={"https://www.fda.gov/files/covid_foodretail_bestpractices_header.png"}  />
+        <div>
+            <img className={'image'} src={"https://www.fda.gov/files/covid_foodretail_bestpractices_header.png"}  />
         </div>
     )
 }
